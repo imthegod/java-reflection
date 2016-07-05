7d914dcbe4202e8b95ac7382ea0ff421161d0075
imthegod <pardhasaradhi108@gmail.com>
2016-07-06 02:00:56 +0530

    Added files

---
+++ src/main/java/base/IRequestBuilder.java
@@ -0,0 +1,13 @@
+package base;
+
+import org.json.JSONObject;
+
+/**
+ * Created by pardhamavilla on 4/7/16.
+ */
+
+public interface IRequestBuilder {
+
+
+    JSONObject buildRequest(Integer i);
+}
---
+++ src/main/java/thirdparty/ParserImpl.java
@@ -0,0 +1,16 @@
+package thirdparty;
+
+import base.IParser;
+
+
+/**
+ * Created by pardhamavilla on 4/7/16.
+ */
+public class ParserImpl implements IParser {
+
+    public void parseRequest(Integer i) {
+        System.out.println("Parsing Response "+i);
+    }
+
+
+}

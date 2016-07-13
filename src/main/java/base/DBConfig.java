af8459e5ef3d5fd3cdb83693cb629a6de319661b
imthegod <pardhasaradhi108@gmail.com>
2016-07-13 12:28:49 +0530

    Made DB Connection SingleTon

---
+++ src/main/java/base/DBConfig.java
@@ -0,0 +1,56 @@
+package base;
+
+/**
+ * Created by pardhamavilla on 13/7/16.
+ */
+public class DBConfig {
+
+    private String url;
+
+    private String username;
+
+    private String password;
+
+    public DBConfig() {
+    }
+
+    public DBConfig(String url, String username, String password) {
+        this.url = url;
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("DBConfig{");
+        sb.append("url='").append(url).append('\'');
+        sb.append(", username='").append(username).append('\'');
+        sb.append(", password='").append(password).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
